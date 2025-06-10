1a9ca7c7ed170e11b052bf0ce785945a0f46e3dc
King123XZ <yersonorosco2002@gmail.com>
2025-06-10 15:27:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ function generarCodigo() {
 const codeBox = document.getElementById("codeBox");
 const connectBtn = document.getElementById("connectBtn");
 const codigo = generarCodigo();
-codeBox.innerText = codigo;
+codeBox.innerText = codigo; // Elimina el div de intro después de la animación
+setTimeout(() => {
+  const intro = document.getElementById("intro");
+  if (intro) {
+    intro.style.display = "none";
+  }
+}, 4000); // Tiempo en milisegundos (3s + 1s animación)
+
 
 connectBtn.addEventListener("click", () => {
   alert(`Tu código único es: ${codigo}. Copialo y pégalo en WhatsApp con el bot para vincularte.`);
